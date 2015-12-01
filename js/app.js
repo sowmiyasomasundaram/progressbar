c364721ae9f496cd001ec404e17720e74eabee74
sowmiyasomasundaram <sowmiyasomu@gmail.com>
2015-12-01 19:55:05 +0530

    Minified css/increment-decrement bug fixes

--- js/app.js
+++ js/app.js
@@ -5,35 +5,36 @@ app.controller('myCtrl', function ($scope) {
     /**option values**/
     $scope.sample = [{
         id: '1',
-        name: 'ProgressBar1'
+        name: 'ProgressBar1',
     },  {
         id: '2',
-        name: 'ProgressBar2'
+        name: 'ProgressBar2',
     }, {
         id: '3',
-        name: 'ProgressBar3'
+        name: 'ProgressBar3',	
     }];
     $scope.selected_item = $scope.sample[0];
     /**add/delete fn**/
-
       $scope.changeProgress = function (valclk, signMath) {
-        var sel_value = $scope.selected_item.name;
-        var old_wid = $("#" + sel_value).find(".progress-bar").width() / $("#" + sel_value).find(".progress-bar").parent().width() * 100;
+       var sel_value = $scope.selected_item.name;
+	   var old_wid = $("#" + sel_value).find("span").text();
         if (signMath == 'plus') {
           //  alert(valclk);
             if(valclk == 10){
-            var t =Math.round(old_wid+10);
-            var new_wid =Math.round(old_wid+10)+"%";
+			t1= parseInt(old_wid);
+            t =Math.round(t1+10);
+            new_wid =t+"%";
             $("#"+sel_value).find(".progress-bar").width(new_wid);
-            $("#"+sel_value).find(".progress_value").html(new_wid);
+            $("#"+sel_value).find("span").html(new_wid);
             if(t >= 100){$("#"+sel_value).find(".progress-bar").css("background-color","green");}
             else{$("#"+sel_value).find(".progress-bar").css("background-color","#f0ad4e");}
             }
             else{
-            var t =Math.round(old_wid+25);
-            var new_wid = Math.round(old_wid+25)+"%";
+			t1= parseInt(old_wid);
+            t =Math.round(t1+25);
+            new_wid =t+"%";
             $("#"+sel_value).find(".progress-bar").width(new_wid);
-            $("#"+sel_value).find(".progress_value").html(new_wid);
+            $("#"+sel_value).find("span").html(new_wid);
             if(t >= 100){$("#"+sel_value).find(".progress-bar").css("background-color","green");}
           //  alert(signMath);
         }
@@ -41,20 +42,22 @@ app.controller('myCtrl', function ($scope) {
          else if (signMath == 'minus'){
           //   alert(valclk);
            if(valclk == 10){
-           var t =Math.round(old_wid-10);
-           var new_wid = Math.round(old_wid-10)+"%";
+		   t1= parseInt(old_wid);
+           t =Math.round(t1-10);
+           new_wid =t+"%";
            $("#"+sel_value).find(".progress-bar").width(new_wid);
            if(t < 100){$("#"+sel_value).find(".progress-bar").css("background-color","#F0AD4E");}
-           if(new_wid < 0+"%"){$("#"+sel_value).find(".progress_value").html("0%");$("#"+sel_value).find(".progress-bar").width("0");}
-           else $("#"+sel_value).find(".progress_value").html(new_wid);
+           if(new_wid < 0+"%"){$("#"+sel_value).find("span").html("0%");$("#"+sel_value).find(".progress-bar").width("0");}
+           else $("#"+sel_value).find("span").html(new_wid);
          }
            else{
-           var t =Math.round(old_wid-25);
-           var new_wid =  Math.round(old_wid-25)+"%";
+          t1= parseInt(old_wid);
+          t =Math.round(t1-25);
+           new_wid =t+"%";
            $("#"+sel_value).find(".progress-bar").width(new_wid);
            if(t < 100){$("#"+sel_value).find(".progress-bar").css("background-color","#F0AD4E");}
-           if(new_wid < 0+"%"){$("#"+sel_value).find(".progress_value").html("0%");$("#"+sel_value).find(".progress-bar").width("0");}
-           else $("#"+sel_value).find(".progress_value").html(new_wid);}
+           if(new_wid < 0+"%"){$("#"+sel_value).find("span").html("0%");$("#"+sel_value).find(".progress-bar").width("0");}
+           else $("#"+sel_value).find("span").html(new_wid);}
           // alert(new_wid);
           // alert(signMath);
         }
